0d639bc0bd9ed53b281b3c3ec01d961598f94209
Matt <mlewer@hotmail.co.uk>
2024-06-30 20:52:17 +0100

    Demo

--- App.tsx
+++ App.tsx
@@ -1,118 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Suspense, useMemo} from 'react';
+import {Canvas, useLoader} from '@react-three/fiber/native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+// https://github.com/pmndrs/react-three-fiber/issues/3085#issuecomment-1837583297
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader';
+import * as THREE from 'three';
+function A() {
+  const buffer = useLoader(THREE.FileLoader, require('./assets/tree_obj.obj'));
+  const obj = useMemo(
+    () => new OBJLoader().parse(THREE.LoaderUtils.decodeText(buffer)),
+    [buffer],
   );
+  return <primitive object={obj} scale={10} />;
 }
+
+// Likely indicates a deeper regression since the start of the thread
+// https://github.com/pmndrs/react-three-fiber/issues/3085#issuecomment-1925877969
+import {Asset} from 'expo-asset';
+function B() {
+  const buffer = useLoader(
+    THREE.FileLoader,
+    Asset.fromModule(require('./assets/tree_obj.obj')).uri,
+  );
+  const obj = useMemo(
+    () => new OBJLoader().parse(THREE.LoaderUtils.decodeText(buffer)),
+    [buffer],
+  );
+  return <primitive object={obj} scale={10} />;
+}
+
+// https://docs.pmnd.rs/react-three-fiber/getting-started/installation#react-native
+import {useGLTF} from '@react-three/drei/native';
+import modelPath from './assets/tree_gltf.gltf';
+function C(props) {
+  const gltf = useGLTF(modelPath);
+  return <primitive {...props} object={gltf.scene} />;
+}
+
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Canvas>
+      <ambientLight />
+      <Suspense>
+        {/* <A/> */}
+        {/* <B /> */}
+        <C />
+      </Suspense>
+    </Canvas>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
