51dd3bbb6c144b984573f278c7c6d9386ace6c36
Matt <mlewer@hotmail.co.uk>
2024-06-30 20:56:42 +0100

    Move imports to top for readability

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Suspense, useMemo} from 'react';
 import {Canvas, useLoader} from '@react-three/fiber/native';
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader';
+import {useGLTF} from '@react-three/drei/native';
+import {Asset} from 'expo-asset';
+import * as THREE from 'three';
+import modelPath from './assets/tree_gltf.gltf';
 
 // https://github.com/pmndrs/react-three-fiber/issues/3085#issuecomment-1837583297
-import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader';
-import * as THREE from 'three';
 function A() {
   const buffer = useLoader(THREE.FileLoader, require('./assets/tree_obj.obj'));
   const obj = useMemo(
@@ -16,7 +19,6 @@ function A() {
 
 // Likely indicates a deeper regression since the start of the thread
 // https://github.com/pmndrs/react-three-fiber/issues/3085#issuecomment-1925877969
-import {Asset} from 'expo-asset';
 function B() {
   const buffer = useLoader(
     THREE.FileLoader,
@@ -30,13 +32,10 @@ function B() {
 }
 
 // https://docs.pmnd.rs/react-three-fiber/getting-started/installation#react-native
-import {useGLTF} from '@react-three/drei/native';
-import modelPath from './assets/tree_gltf.gltf';
 function C(props) {
   const gltf = useGLTF(modelPath);
   return <primitive {...props} object={gltf.scene} />;
 }
-
 
 function App(): React.JSX.Element {
   return (
